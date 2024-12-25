afaeb7b1679ca4e98b9b7be1231b2820478f3dd4
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-25 23:56:04 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-05c172c7f0d3aed00" # Ubuntu AMI
   instance_type = "t2.micro"
 
   tags = {
