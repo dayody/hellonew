674a06c96b95bc560015851845322e5ffa3a518b
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-25 22:21:01 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,10 +7,8 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  
+  region     = "eu-west-2"
 }
 
 resource "aws_instance" "example" {
@@ -24,3 +22,22 @@ resource "aws_instance" "example" {
 
 output "instance_public_ip" {
   value = aws_instance.example.public_ip
+}
+
+resource "aws_security_group" "allow_http" {
+  name_prefix = "allow_http"
+  description = "Allow HTTP traffic"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
