303c4b6f5d828b157cfea0b68c6a4965d3a01a57
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 09:32:07 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,22 +21,28 @@ resource "aws_subnet" "subnet" {
   availability_zone = "eu-west-2a"
 }
 
-resource "aws_security_group" "ecs" {
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "allow_tls"
   }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_ecs_cluster" "main" {
