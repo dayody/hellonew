0ccb2a9056d1e77c9ee1c930e73e3454b578124e
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 10:42:03 +0000

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,16 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-west-2"
+}
+
 # Reference the existing IAM role
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
 
-# First IAM Role Policy Attachment
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_1" {
+# Attach the correct policy
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = data.aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# Second IAM Role Policy Attachment (if needed)
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_2" {
-  role       = data.aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerServiceforEC2Role"
 }
