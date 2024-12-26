4940b46d88c937671fdf37c0ae093207423e0e20
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 13:14:10 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -76,8 +76,8 @@ resource "aws_ecs_cluster" "main" {
   name = "ecs-cluster"
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+resource "aws_iam_role" "ecs_task_execution_role_alternate" {
+  name = "ecsTaskExecutionRoleAlternate"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -90,8 +90,8 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_alternate_policy" {
+  role       = aws_iam_role.ecs_task_execution_role_alternate.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
