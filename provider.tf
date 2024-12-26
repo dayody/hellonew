7d598647d1dc936ce22d56bad574da9503c52e69
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 10:42:32 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
- 
-provider "aws" {
-  region     = "eu-west-2"
-}
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -56,26 +43,6 @@ resource "aws_security_group" "ecs" {
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = "hello-cluster"
-}
-
-# IAM Role for ECS Task Execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS Task Definition
@@ -126,10 +93,11 @@ resource "aws_ecs_service" "hello_service" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.hello_task.arn
   desired_count   = 1
+  launch_type     = "EC2"
 
   network_configuration {
     subnets          = [aws_subnet.subnet.id]
     security_groups  = [aws_security_group.ecs.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 }
