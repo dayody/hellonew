ff2794bed0a1d5afcdd21cb2a7837bc5439c600f
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 10:35:06 +0000

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -3,7 +3,14 @@ data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+# First IAM Role Policy Attachment
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_1" {
   role       = data.aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Second IAM Role Policy Attachment (if needed)
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_2" {
+  role       = data.aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerServiceforEC2Role"
 }
