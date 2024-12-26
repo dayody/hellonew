b5ec270afe4c0ecef46f7227a899053769c332ca
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 13:36:37 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
