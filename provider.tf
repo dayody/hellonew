789d42af3c63fe3a216684323c2b7f076e51bde8
Adedayo Ademuyiwa <59888003+dayody@users.noreply.github.com>
2024-12-26 12:50:40 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,56 @@
-# VPC
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
-# Subnet
-resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-2a"
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
-# Security Group for ECS Instances
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_security_group" "ecs" {
   vpc_id = aws_vpc.main.id
-
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -27,79 +61,65 @@ resource "aws_security_group" "ecs" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
-    Name = "ecs_security_group"
+    Name = "ecs-sg"
   }
 }
 
-# ECS Cluster
 resource "aws_ecs_cluster" "main" {
-  name = "hello-cluster"
+  name = "ecs-cluster"
 }
 
-# ECS Task Definition
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "hello_task" {
   family                   = "hello-task"
   network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+  requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "hello-app"
-      image     = "851725211248.dkr.ecr.eu-west-2.amazonaws.com/hellonew:cbca0212ba368d648cf2acca90930470a7cb7767"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = "hello-app"
+    image     = "your-docker-image-url"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
 }
 
-# EC2 Instances for ECS Cluster
 resource "aws_instance" "ecs_instances" {
   count         = 2
   ami           = "ami-05c172c7f0d3aed00" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet.id
-  security_groups = [aws_security_group.ecs.id]
-
-  associate_public_ip_address = true  # Ensures EC2 instances get public IPs
-
-  lifecycle {
-    create_before_destroy = true
-  }
- resource "aws_autoscaling_group" "ecs" {
-  launch_configuration = aws_launch_configuration.ecs.id
-  min_size             = 1
-  max_size             = 3
-  desired_capacity     = 1
-
-  vpc_zone_identifier = ["subnet-0123456789abcdef0"]
-}
-
-  security_groups = [aws_security_group.ecs.id]
-}
+  subnet_id     = aws_subnet.public_subnet.id
+  security_group = aws_security_group.ecs.id  # Corrected the security group attribute
 
   user_data = <<-EOF
               #!/bin/bash
@@ -111,7 +131,6 @@ resource "aws_instance" "ecs_instances" {
   }
 }
 
-# ECS Service
 resource "aws_ecs_service" "hello_service" {
   name            = "hello-service"
   cluster         = aws_ecs_cluster.main.id
@@ -120,8 +139,8 @@ resource "aws_ecs_service" "hello_service" {
   launch_type     = "EC2"
 
   network_configuration {
-    subnets          = [aws_subnet.subnet.id]
+    subnets          = [aws_subnet.public_subnet.id]
     security_groups  = [aws_security_group.ecs.id]
-    assign_public_ip = false
+    assign_public_ip = true
   }
 }
